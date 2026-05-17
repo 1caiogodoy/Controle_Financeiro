3b5e333f1f618de441ea451124aefba2574a55ad
Caio Godoy <caiogodoaraujo@gmail.com>
2026-05-17 18:26:25 -0300

    feat: integração com API financeira

--- testes/testes_financeiro.py
+++ testes/testes_financeiro.py
@@ -1,4 +1,5 @@
 from main import calcular_sobra
+import requests
 
 def test_sobra_correta():
     sobra = calcular_sobra(1000, 200, 200, 100, 100)
@@ -7,3 +8,14 @@ def test_sobra_correta():
 def test_sobra_negativa():
     sobra = calcular_sobra(500, 300, 300, 100, 100)
     assert sobra < 0
+
+
+def test_api_dolar():
+    resposta = requests.get(
+        "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+    )
+
+    dado = resposta.json()
+
+    assert resposta.status_code == 200
+    assert 'USDBRL' in dado
